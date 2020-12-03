5174d67fc6c8f0a33e1eaff37f95a55ec8fa0aa0
Mario Pérez <mario@geekytheory.com>
2020-12-03 08:43:49 +0100

    Day 03: solve problem part 2

--- day_03/app.py
+++ day_03/app.py
@@ -1,21 +1,31 @@
-from typing import List
+from typing import List, Tuple
 
 
-def count_trees(map_to_airport: List[str]) -> int:
+def count_trees(map_to_airport: List[str], move_positions_right: int, move_positions_down: int) -> int:
     height = len(map_to_airport)
     total_trees = 0
     current_vertical_position = 0
     current_horizontal_position = 0
     path_width = len(map_to_airport[0])
     while current_vertical_position < height - 1:
-        current_vertical_position += 1
-        current_horizontal_position += 3
+        current_vertical_position += move_positions_down
+        current_horizontal_position += move_positions_right
+
         if current_horizontal_position >= path_width:
             current_horizontal_position = current_horizontal_position - path_width
+
         if map_to_airport[current_vertical_position][current_horizontal_position] == '#':
             total_trees += 1
 
     return total_trees
+
+
+def multiply_count_trees_for_multiple_direction_options(map_to_airport: List[str], directions: List[Tuple]) -> int:
+    multiplication = 1
+    for move_position_right, move_position_down in directions:
+        multiplication = multiplication * count_trees(map_to_airport, move_position_right, move_position_down)
+
+    return multiplication
 
 
 def get_input() -> List[str]:
@@ -31,4 +41,7 @@ if __name__ == '__main__':
     entries = get_input()
     map_to_airport = get_input()
     print("Solution to first problem of day 03:")
-    print(count_trees(map_to_airport))
+    print(count_trees(map_to_airport, 3, 1))
+
+    print("Solution to second problem of day 03:")
+    print(multiply_count_trees_for_multiple_direction_options(map_to_airport, [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]))
--- day_03/tests/test_app.py
+++ day_03/tests/test_app.py
@@ -1,5 +1,5 @@
 import unittest
-from day_03.app import count_trees
+from day_03.app import count_trees, multiply_count_trees_for_multiple_direction_options
 
 
 class AppTestCase(unittest.TestCase):
@@ -18,9 +18,36 @@ class AppTestCase(unittest.TestCase):
             '.#..#...#.#',
         ]
 
-        total_trees = count_trees(path)
+        total_trees_option_one = count_trees(path, 1, 1)
+        total_trees_option_two = count_trees(path, 3, 1)
+        total_trees_option_three = count_trees(path, 5, 1)
+        total_trees_option_four = count_trees(path, 7, 1)
+        total_trees_option_five = count_trees(path, 1, 2)
 
-        self.assertEquals(7, total_trees)
+        self.assertEquals(2, total_trees_option_one)
+        self.assertEquals(7, total_trees_option_two)
+        self.assertEquals(3, total_trees_option_three)
+        self.assertEquals(4, total_trees_option_four)
+        self.assertEquals(2, total_trees_option_five)
+
+    def test_count_trees_for_multiple_direction_options(self):
+        path = [
+            '..##.......',
+            '#...#...#..',
+            '.#....#..#.',
+            '..#.#...#.#',
+            '.#...##..#.',
+            '..#.##.....',
+            '.#.#.#....#',
+            '.#........#',
+            '#.##...#...',
+            '#...##....#',
+            '.#..#...#.#',
+        ]
+
+        result = multiply_count_trees_for_multiple_direction_options(path, [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)])
+
+        self.assertEquals(336, result)
 
 
 if __name__ == '__main__':
